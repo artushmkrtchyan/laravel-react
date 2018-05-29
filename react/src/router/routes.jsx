fcf5416f93e86bb8a3937c581c9fbb52b09cad3d
Artush <arthush01@gmail.com>
2018-05-29 10:27:35 +0400

    routes.jsx

--- react/src/router/routes.jsx
+++ react/src/router/routes.jsx
@@ -23,3 +23,37 @@ export default (<Route path="/">
 	<Route path="/edit-post/:postId" component={EditPosts} />
 
 </Route>)
+
+
+
+// import React from 'react';
+// import {BrowserRouter, Route, Switch} from 'react-router-dom';
+
+// import Home from '../pages/home.jsx';
+// import Account from '../pages/account.jsx';
+// import Posts from '../pages/posts.jsx';
+// import Post from '../pages/post.jsx';
+// import Products from '../pages/products.jsx';
+// import Product from '../pages/product.jsx';
+// import AddPosts from '../pages/addPost.jsx';
+// import EditPosts from '../pages/editPost.jsx';
+
+
+// const Router = ({ store }) =>(
+//     <Provider store={store}>
+//       <BrowserRouter>
+//       <Switch>
+//         <Route exact path="/" component={Home}/>
+// 		<Route path="/account" component={Account} />
+// 		<Route path="/posts" component={Posts} />
+// 		<Route path="/post/:postId" component={Post} />
+// 		<Route path="/products" component={Products} />
+// 		<Route path="/product/:productID" component={Product} />
+// 		<Route path="/add-post" component={AddPosts} />
+// 		<Route path="/edit-post/:postId" component={EditPosts} />
+//       </Switch>
+//     </BrowserRouter>
+//     </Provider>
+// );
+
+// export default Router;
